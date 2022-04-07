5788914d556a3b5f61b902d43d4e862143dd00dd
Christian N Leiva <chriistiianleiiva@gmail.com>
2022-04-06 22:06:46 -0300

    agregando codigo de la rama feature pa que ande

---
+++ DepositoServicesLibrary/Controllers/UbicacionController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using DepositoClassLibrary.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepositoServicesLibrary.Controllers
+{
+    public static class UbicacionController
+    {
+        private static SqliteDataAccess<UbicacionesEstadosJuegosDTO> dataAccess = new SqliteDataAccess<UbicacionesEstadosJuegosDTO>();
+
+        public static UbicacionesEstadosJuegosDTO saveAndGet(UbicacionesEstadosJuegosDTO ubicacionesEstadosJuegosDTO)
+        {
+            int id = dataAccess.save(ubicacionesEstadosJuegosDTO);
+            return getOne(id);
+        }
+
+        public static UbicacionesEstadosJuegosDTO getOne(int id)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@Id", id);
+            UbicacionesEstadosJuegosDTO ubicacionesEstadosJuegosDTO = dataAccess.getOne("id = @Id", parameters);
+            if (ubicacionesEstadosJuegosDTO == null)
+            {
+                throw new Exception("No existe");
+            }
+
+            return ubicacionesEstadosJuegosDTO;
+        }
+    }
+}
